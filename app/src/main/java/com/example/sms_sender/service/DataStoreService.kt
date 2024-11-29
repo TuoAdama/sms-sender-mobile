a7e5f1efaf1db0f4fa4bbbfbf0e6da5241708da3
TuoAdama <tuoadama17@gmail.com>
2024-11-29 14:02:40 +0100

    fix: preference data

--- app/src/main/java/com/example/sms_sender/service/DataStoreService.kt
+++ app/src/main/java/com/example/sms_sender/service/DataStoreService.kt
@@ -11,30 +11,32 @@ import com.example.sms_sender.service.setting.SettingKey
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("settings");
+
 class DataStoreService(private val context: Context)
 {
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("settings");
+    private val dataStore = context.dataStore;
 
     suspend fun saveString(key: String, value: String) {
-        context.dataStore.edit {
+        dataStore.edit {
             it[stringPreferencesKey(key)] = value
         }
     }
 
     suspend fun getString(key: String): String? {
-        return context.dataStore.data.map {
+        return dataStore.data.map {
             it[stringPreferencesKey(key)]
         }.first()
     }
 
     suspend fun getInt(key: String): Int? {
-        return context.dataStore.data.map {
+        return dataStore.data.map {
             it[intPreferencesKey(key)]
         }.first()
     }
 
     suspend fun saveInt(key: String, value: Int) {
-        context.dataStore.edit {
+        dataStore.edit {
             it[intPreferencesKey(key)] = value
         }
     }
