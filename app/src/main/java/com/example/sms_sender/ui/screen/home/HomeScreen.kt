02b9bc170e3a8982295383cc3a7ac33c39f7563c
TuoAdama <tuoadama17@gmail.com>
2025-02-09 16:19:44 +0100

    feat: retreive database rows

--- app/src/main/java/com/example/sms_sender/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/sms_sender/ui/screen/home/HomeScreen.kt
@@ -8,11 +8,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sms_sender.R
 import com.example.sms_sender.ui.components.InfoSection
+import com.example.sms_sender.ui.components.SmsMessageList
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,7 +24,7 @@ fun HomeScreen(
     homeViewModel: HomeViewModel = viewModel(factory = HomeViewModel.Factory)
 ){
 
-    val homeUiState = homeViewModel.homeUiState
+    val homeUiState = homeViewModel.homeUiState.collectAsState().value
 
     Scaffold(
         topBar = {
@@ -33,6 +37,12 @@ fun HomeScreen(
         content = { padding ->
             Column(modifier = Modifier.padding(padding)) {
                 InfoSection(homeUiState)
+                Text(
+                    modifier = Modifier.padding(0.dp, 18.dp),
+                    fontSize = 25.sp,
+                    text = stringResource(R.string.messages)
+                )
+                SmsMessageList(homeUiState.messages)
             }
         }
     )
--- app/src/main/java/com/example/sms_sender/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/example/sms_sender/ui/screen/home/HomeViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.sms_sender.ui.screen.home
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
@@ -11,20 +9,25 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.sms_sender.App
 import com.example.sms_sender.data.repository.SmsDataRepository
+import com.example.sms_sender.model.SmsData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class HomeViewModel(private val smsDataRepository: SmsDataRepository) : ViewModel() {
 
-    var homeUiState  by mutableStateOf(HomeUiState())
-    private set
+    private val _homeUiState = MutableStateFlow(HomeUiState())
+
+    val homeUiState: StateFlow<HomeUiState> = _homeUiState
 
     init {
         viewModelScope.launch {
-            homeUiState = HomeUiState(
+            _homeUiState.value =  HomeUiState(
                 numOfSmsSent = smsDataRepository.getCountSmsSent().first(),
                 numOfUnSmsSent = smsDataRepository.getCountUnSmsSent().first(),
                 totalSms = smsDataRepository.count().first(),
+                messages = smsDataRepository.getItems().first()
             )
         }
     }
@@ -43,5 +46,6 @@ class HomeViewModel(private val smsDataRepository: SmsDataRepository) : ViewMode
 data class HomeUiState(
     val numOfSmsSent: Int = 0,
     val numOfUnSmsSent: Int = 0,
-    val totalSms: Int = 0
+    val totalSms: Int = 0,
+    val messages: List<SmsData> = listOf(),
 )
