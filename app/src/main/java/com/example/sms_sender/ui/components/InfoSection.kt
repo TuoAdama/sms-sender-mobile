e8a308ec6ec77a3610449ef35cff450569f60427
TuoAdama <tuoadama17@gmail.com>
2025-02-23 20:17:09 +0100

    feat: added disable button opacity

--- app/src/main/java/com/example/sms_sender/ui/components/InfoSection.kt
+++ app/src/main/java/com/example/sms_sender/ui/components/InfoSection.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
