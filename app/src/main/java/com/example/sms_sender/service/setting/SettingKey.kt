f1be47d6b5f9110a463455dccc9d516e48a472eb
TuoAdama <tuoadama17@gmail.com>
2025-03-27 23:11:34 +0100

    refactor: second

--- app/src/main/java/com/example/sms_sender/service/setting/SettingKey.kt
+++ app/src/main/java/com/example/sms_sender/service/setting/SettingKey.kt
@@ -7,5 +7,5 @@ object Setting {
     const val API_URL_KEY = "API_URL"
     const val API_IS_AUTHENTICATED_KEY = "API_IS_AUTHENTICATED"
     const val API_TOKEN_KEY = "API_TOKEN";
-    const val SMS_BETWEEN_EACH_SENDING_SMS = 5_000L; // 10s
+    const val SMS_BETWEEN_EACH_SENDING_SMS = 5_000L; // 5s
 }
